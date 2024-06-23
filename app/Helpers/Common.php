41baa41fbfabd05a0c12a5e3c9c96e13efe6e9bd
nnebuchi <nnebuchiosigbo340@gmail.com>
2024-06-23 09:01:22 +0100

    optimising shipment

---
+++ app/Helpers/Common.php
@@ -0,0 +1,67 @@
+<?php
+
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Str;
+use App\Models\Schedule;
+use Illuminate\Support\Facades\Auth;
+
+if (!function_exists('generateOTP')) {
+
+    function generateOTP()
+    {   
+        // Generates Random number between given pair
+        return mt_rand(10000, 99999);
+    }
+
+    function slugify(String $string)
+    {
+        return str_replace(' ', '-', $string).'-'.Str::random(5);
+    }
+
+    /**
+     * Generate reference for payments
+     *
+     * @param  Int $id of the patient
+     * @return String
+    */
+    function generateReference(int $id)
+    {
+        $token = "";
+        $codeAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $codeAlphabet .= 'abcdefghijklmnopqrstuvwxyz';
+        $codeAlphabet .= '0123456789';
+        $max = strlen($codeAlphabet) - 1;
+        for($i=0; $i<14; $i++):
+            $token .= $codeAlphabet[mt_rand(0, $max)]; 
+        endfor; 
+        return $id.strtolower($token);
+    }
+
+    /**
+     * Get the distance between two coordinates in kilometres/miles
+     *
+     * @param  Double latitude A
+     * @param  Double longitude A
+     * @param  Double latitude B
+     * @param  Double longitude B
+     * @return Int
+    */
+    function getDistance($latA, $lngA, $latB, $lngB)
+    {
+        $R = 6371000;
+        $radiansLAT_A = deg2rad($latA);
+        $radiansLAT_B = deg2rad($latB);
+        $variationLAT = deg2rad($latB - $latA);
+        $variationLNG = deg2rad($lngB - $lngA);
+
+        $a = sin($variationLAT / 2) * sin($variationLAT / 2)
+        + cos($radiansLAT_A) * cos($radiansLAT_B) * sin($variationLNG / 2) * sin($variationLNG / 2);
+
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+
+        $d = $R * $c;
+
+        return $d / 1000;
+    }
+}
--- app/Services/ShippingService.php
+++ app/Services/ShippingService.php
@@ -3,11 +3,25 @@ namespace App\Services;
 
 use App\Mail\ShippingReportMail;
 use Illuminate\Support\Facades\Mail;
+use App\Models\{User, Shipment};
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
 
 class ShippingService
 {
     public static function reportShippingUpdate($mail_data){
         Mail::to(env("SHIPPING_REPORT_MAIL"))->send(new ShippingReportMail($mail_data));
         // Mail::to([env("SHIPPING_REPORT_MAIL"), env("ADMIN_MAIL")])->send(new ShippingReportMail($mail_data));
+    }
+
+    public static function saveShipment(Request $request){
+        $user = User::find(Auth::user()->id);
+        $shipment = new Shipment;
+        $shipment->user_id = $user->id;
+        $shipment->title = sanitize_input($request->title);
+        $shipment->slug = slugify($shipment->title);
+        $shipment->save();
+        return $shipment;
     }
 }
